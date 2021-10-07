bd6a2ef7b3f0e65e0871cd3e7f5b7fbbf8171223
PakulskiAntunes <emerson.antunes@rede.ulbra.br>
2021-10-07 18:41:02 -0300

    o

--- src/App.java
+++ src/App.java
@@ -29,7 +29,7 @@ public class App {
                     System.out.print(" | "+campo[i][j]);
                 }
             }
-            System.out.println(" |-");
+            System.out.println(" |");
         }
     }
 }
